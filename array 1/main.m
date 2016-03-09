a7782f58210872444ad972847b4b8f3a282cab05
Adrian Cummins <adrian.cummins@gmail.com>
2016-03-09 10:09:50 +0000

    Initial Commit

---
+++ array 1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  array 1
+//
+//  Created by Ade Cummins on 09/03/2016.
+//  Copyright © 2016 Ade Cummins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
